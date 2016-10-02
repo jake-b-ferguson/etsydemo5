171faa0332d98473e3154d26483dd1ab65e229b4
Jake <jake.b.ferguson@gmail.com>
2016-10-02 16:11:02 -0500

    Associate listings with users and remove broken aws functionality

--- Gemfile
+++ Gemfile
@@ -10,7 +10,6 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem "paperclip", "~> 5.0.0"
-gem 'aws-sdk', '~> 2.3.0'
 gem "figaro"
 gem 'devise'
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,16 +1,11 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.production?
 		has_attached_file :image, styles: { thumb: '100x100>', square: '200x200#', medium: '300x300>' }, validate_media_type: false, default_url: "default.jpg"
-	else
-		has_attached_file :image, styles: { thumb: '100x100>', square: '200x200#', medium: '300x300>' }, validate_media_type: false, default_url: "default.jpg",
-		  storage: :s3,
-  		  s3_protocol: 'http',
-  		  s3_credentials: Rails.root.join("config/application.yml"),
-  		  path: "/:class/:attachment/:id_partition/:style/:filename"
-		
-	end
+		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 
 	validates :name, :description, :price, presence: true
 	validates :price, numericality: { greater_than: 0 }
 	validates_attachment_presence :image
+
+	belongs_to :user
 end
