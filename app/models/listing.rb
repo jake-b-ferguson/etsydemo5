be8edcb326e016d4a615a698c408bc2409a1901e
Jake <jake.b.ferguson@gmail.com>
2016-10-02 01:32:57 -0500

    maybe it will work someday

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, styles: { thumb: '100x100>', square: '200x200#', medium: '300x300>' }, default_url: "default.jpg"
+	has_attached_file :image, styles: { thumb: '100x100>', square: '200x200#', medium: '300x300>' }, validate_media_type: false, default_url: "default.jpg"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 
--- db/migrate/20160928061151_add_image_to_listing.rb
+++ db/migrate/20160928061151_add_image_to_listing.rb
@@ -1,9 +1,2 @@
 class AddImageToListing < ActiveRecord::Migration
- def self.up
-    add_attachment :listing, :image
-  end
-
-  def self.down
-    remove_attachment :listing, :image
-  end
 end
