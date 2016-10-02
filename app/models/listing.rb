7bd1ebb59927fe88954fab2bd81bda311d814f4b
Jake <jake.b.ferguson@gmail.com>
2016-10-02 04:13:52 -0500

    Add validations and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,16 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, styles: { thumb: '100x100>', square: '200x200#', medium: '300x300>' }, validate_media_type: false, default_url: "default.jpg"
-	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	if Rails.env.production?
+		has_attached_file :image, styles: { thumb: '100x100>', square: '200x200#', medium: '300x300>' }, validate_media_type: false, default_url: "default.jpg"
+	else
+		has_attached_file :image, styles: { thumb: '100x100>', square: '200x200#', medium: '300x300>' }, validate_media_type: false, default_url: "default.jpg",
+		  storage: :s3,
+  		  s3_protocol: 'http',
+  		  s3_credentials: Rails.root.join("config/application.yml"),
+  		  path: "/:class/:attachment/:id_partition/:style/:filename"
+		
+	end
 
-
-	belongs_to :user
+	validates :name, :description, :price, presence: true
+	validates :price, numericality: { greater_than: 0 }
+	validates_attachment_presence :image
 end
