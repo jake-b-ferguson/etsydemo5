44e0ed20fb4860dc6ee811227b53662217b86a26
Jake <jake.b.ferguson@gmail.com>
2016-10-02 03:45:06 -0500

    Add name field to users

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
-Paperclip::Attachment.default_options[:url] = ':etsydemo2016jf.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
